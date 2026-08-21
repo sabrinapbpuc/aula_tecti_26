5bc4f51a5c3cc028146bfb8b7e4f9faa38dc885b
sabrinapbpuc <sabrina.pb@puccampinas.edu.br>
2026-08-21 09:25:33 -0300

    correção de quebra de linha

--- aula.py
+++ aula.py
@@ -1,7 +1,7 @@
 # projeto 1
 
 nome = input("Digite seu nome" )
-print(f"Seu nome é {nome} .../n bom dia")
+print(f"Seu nome é {nome} ...\n bom dia")
 idade = input("Digite sua idade ")
 print(f"Sua idade é{idade}")
 
